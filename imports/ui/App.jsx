838477d5e3c8a20d04da0379a30c9fd9e5405b3d
Daniel Chen <dchen12812@gmail.com>
2016-08-01 20:02:16 -0700

    change hash

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,9 +6,10 @@ import Post from './Post.jsx';
 export default class App extends Component {
   getPosts() {
     return [
-      { _id: 1, user_id: 1, timeCreated: Date.now(), text: 'This is task 1' },
-      { _id: 2, user_id: 1, timeCreated: Date.now(), text: 'This is task 2' },
-      { _id: 3, user_id: 1, timeCreated: Date.now(), text: 'This is task 3' },
+      { _id: 1, user_id: 1, timeCreated: new Date("October 13, 2014 11:13:00"), text: 'Hello?' },
+      { _id: 2, user_id: 1, timeCreated: new Date("November 20, 2015 08:00:00"), text: 'Anyone here?' },
+      { _id: 3, user_id: 1, timeCreated: new Date("July 30, 2016 08:13:00"), text: "I'm a robot" },
+      { _id: 4, user_id: 2, timeCreated: new Date("August 01, 2016 15:45:00"), text: 'You are a robot' },
     ];
   }
 
@@ -24,7 +25,7 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="container">
+      <div>
         <header>
           <h1>Todo List</h1>
         </header>
